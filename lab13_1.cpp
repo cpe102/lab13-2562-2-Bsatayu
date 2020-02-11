7f5341a5d6e1e52a3c497e651b01f6c91c74cf69
Bsatayu <satayu_s@cmu.ac.th>
2020-02-11 17:46:26 +0700

    lab 13

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -11,19 +11,43 @@ void swap(T d[],int x,int y){
 
 template <typename T>
 void insertionSort(T d[],int N){
-	
-//Write your code here.
+	for (int i=1; i <N; i++){		
+	  for (int j=0;j < N ;j++){
+		  if(i==j){
+          cout<<"["<<d[j]<<"]";
+		  }else{
+		  cout << d[j] <<" ";  
+		  }
+	   }	
+	  int num=i;
+	  for(int k=i;k>0;k--){
+		  if(d[k]>d[k-1]){
+			  swap(d,k,k-1);
+			  num=k-1;
+		  	}
+	   }
+	   cout<<"=> ";
+	   for (int j=0;j < N ;j++){
+		  if(j==num){
+          cout<<"["<<d[j]<<"]"<<" ";
+		  }else{
+		  cout << d[j] <<" ";  
+		  }
+		  
+	   }	
+	   cout<<"\n"; 
 
-}
 
+ 	} 
+}	
 int main(){
-	int a[10] = {12,25,30,44,2,0,4,7,55,25};
+	int d[10] = {12,25,30,44,2,0,4,7,55,25};
 	cout << "Input Array:";
-	for(int i = 0; i < 10; i++) cout << a[i] << " ";
+	for(int i = 0; i < 10; i++) cout << d[i] << " ";
 	cout << "\n\n";
 	
-	insertionSort(a,10);
+	insertionSort(d,10);
 	
 	cout << "\nSorted Array:";
-	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
+	for(int i = 0; i < 10; i++) cout << d[i] << " ";	
 }
